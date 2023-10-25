f481be05c4dabe8875c795df7af56f8d722100af
stanleylee417 <stanleylee417@gmail.com>
2023-10-25 19:36:38 +0800

    新增antStanley頁面

---
+++ classic/src/view/subsystem/antStanley/mainController.js
@@ -0,0 +1,8 @@
+Ext.define('antnex.subsystem.antStanley.mainController', {
+    extend: 'antnex.default.defaultController',
+    alias: 'controller.antStanley-main',
+    requires: [],
+    config: {
+        name: 'Stanley的首頁',
+    },
+});
