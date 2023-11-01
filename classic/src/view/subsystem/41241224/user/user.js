73b4964b2bd470e99ea0bf34e2367cf76c516c39
kyoto48763 <88764412+kyoto48763@users.noreply.github.com>
2023-11-02 00:55:38 +0800

    usercontroller 初始化

--- classic/src/view/subsystem/41241224/user/user.js
+++ classic/src/view/subsystem/41241224/user/user.js
@@ -1,16 +1,18 @@
-Ext.define('antnex.subsystem.41241224.main.main',
+Ext.define('antnex.subsystem.41241224.user.user',
     {
         extend:'Ext.panel.Panel',
         requires:[
-        'antnex.subsystem.41241224.main.mainController',
+        'antnex.subsystem.41241224.user.userController',
         ],
-        alias:'widget.page-41241224-main',
-        controller:'page-41241224-main',
-        title:'41241224的首頁',
+        alias:'widget.page-41241224-user',
+        controller:'page-41241224-user',
+        title:'使用者管理介面',
 
         items:[{
-            html:'41241224的首頁'
+            html:'使用者管理介面'
         }]
+
+
 
     }
 );
--- classic/src/view/subsystem/41241224/user/userController.js
+++ classic/src/view/subsystem/41241224/user/userController.js
@@ -1,4 +1,28 @@
-Ext.define('antnex.subsystem.41241224.main.mainController',{
+Ext.define('antnex.subsystem.41241224.user.userController',{
     extend:'Ext.app.ViewController',
-    alias:'controller.page-41241224-main'
+    alias:'controller.page-41241224-user',
+    //requires:[],
+    //config:{    
+      //  name:'41241224使用者管理介面',
+
+    //},
+    initialization:async function(){
+        let me =this;
+        try{
+            if(e.getKey()==e.ENTER){
+                me.doSearch();
+            }
+        }
+        catch(e){
+            me.showError('userController/ enterSearch error',e);
+        }
+    },
+
+
+
+
+
+
+
+
 });
