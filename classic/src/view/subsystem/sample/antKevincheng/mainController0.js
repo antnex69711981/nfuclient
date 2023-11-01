81b1fa3aab473c069201d409901118a62c53812e
antnex2017 <a0917546998@gmail.com>
2023-11-01 18:21:57 +0800

    mdf

--- classic/src/view/subsystem/sample/antKevincheng/mainController0.js
+++
@@ -1,8 +0,0 @@
-Ext.define('antnex.subsystem.sample.antKevin.mainController', {
-    extend: 'antnex.default.defaultController',
-    alias: 'controller.antKevin-main',
-    requires: [],
-    config: {
-        name: 'Kevin的首頁',
-    },
-});
