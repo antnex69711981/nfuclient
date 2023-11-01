346d2673e8097778e3c94dec6c0d27b8ea57a3f5
stanleylee417 <stanleylee417@gmail.com>
2023-11-01 12:07:32 +0800

    test

--- classic/src/view/subsystem/sample/antStanley/main/main.js
+++ classic/src/view/subsystem/sample/antStanley/main/main.js
@@ -6,7 +6,7 @@ Ext.define('antnex.subsystem.sample.antStanley.main.main', {
     alias: 'widget.antStanley-main',
     controller: 'antStanley-main',
 
-    title: 'Stanley的首頁',
+    title: 'Stanley的首頁12',
 
     layout: {
         type: 'vbox',
