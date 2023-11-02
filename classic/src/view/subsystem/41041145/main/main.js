8fd5bc5c3f9073ab1b1d51b840fc39c503012e47
Lotenjau <148881261+Lotenjau@users.noreply.github.com>
2023-11-02 09:26:50 +0800

    新增使用者首頁與使用者管理

---
+++ classic/src/view/subsystem/41041145/main/main.js
@@ -0,0 +1,14 @@
+Ext.define('antnex.subsystem.41041145.main.main', {
+    extend: 'antnex.default.defaultView',
+    requires: [
+        'antnex.subsystem.41041145.main.mainController',
+    ],
+    alias: 'widget.page-41041145-main',
+    controller: 'page-41041145-main',
+
+    title: '41041145首頁',
+
+    items: [{
+        html: '41041145首頁'
+    }]
+});
