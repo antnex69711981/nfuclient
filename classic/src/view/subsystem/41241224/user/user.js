14994372889a6e1e52b5155b9d44b70f7ea21029
kyoto48763 <88764412+kyoto48763@users.noreply.github.com>
2023-11-09 22:34:28 +0800

    11/9

--- classic/src/view/subsystem/41241224/user/user.js
+++ classic/src/view/subsystem/41241224/user/user.js
@@ -8,10 +8,38 @@ Ext.define('antnex.subsystem.41241224.user.user',
         controller:'page-41241224-user',
         title:'使用者管理介面',
 
-        items:[{
-            html:'使用者管理介面'
-        }]
+        layout:{
+            type:'table',
+            alias:'stretch',
+        },
 
+        DockedItem:[{
+            xtype:'panel',
+            layout:{
+                type:'hbox',
+                alias:'stretch'
+            },
+            dock:'left',
+            margin:0,
+            items:[{
+                type:'toolbar',
+                scrollable:true,
+                border:false,
+                padding:'0 0 0 5',
+                items: [
+                    {
+                        text: '查詢',
+                        xtype: 'button',
+                        height: 30,
+                        width: 60
+                    },
+        
+                ]
+            }]
+
+        }]
+        
+        
 
 
     }
--- classic/src/view/subsystem/41241224/user/userController.js
+++ classic/src/view/subsystem/41241224/user/userController.js
@@ -1,11 +1,6 @@
 Ext.define('antnex.subsystem.41241224.user.userController',{
     extend:'Ext.app.ViewController',
     alias:'controller.page-41241224-user',
-    //requires:[],
-    //config:{    
-      //  name:'41241224使用者管理介面',
-
-    //},
     initialization:async function(){
         let me =this;
         try{
