4e05a260a4830086231ce9fb32e7c5603f532614
briante25 <91589447+briante25@users.noreply.github.com>
2023-11-11 17:23:16 +0800

    新增首頁+使用者介面v1
    可查詢、維護
    功能尚未實行

---
+++ classic/src/view/subsystem/41041124/main/mainController.js
@@ -0,0 +1,8 @@
+Ext.define('antnex.subsystem.41041124.main.mainController', {
+    extend: 'antnex.default.defaultController',
+    alias: 'controller.page-41041124-main',
+    requires: [],
+    config: {
+        name: '範例頁面',
+    },
+});
