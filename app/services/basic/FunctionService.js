7a3779c92cf1a7779c484be571d9866f6e460dbc
sheep1102 <sheep9603@gmail.com>
2023-11-12 21:33:49 +0800

    框架設定完成

--- app/services/basic/FunctionService.js
+++ app/services/basic/FunctionService.js
@@ -57,6 +57,7 @@ Ext.define('antnex.services.basic.FunctionService', {
                 { code: '40141152', name: '鄭凱文' },
                 { code: '11261103', name: '劉宜欣' },
                 { code: '11261119', name: '陳奕潔' },
+                { code: '11261133', name: '楊子庭' },               
                 { code: '40941137', name: '陳朝羿' },
                 { code: '40941139', name: '陳嘉笙' },
                 { code: '41041115', name: '成政軒' },
---
+++ classic/src/view/subsystem/11261133/main/mainController.js
@@ -0,0 +1,4 @@
+Ext.define('antnex.subsystem.11261133.main.mainController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.page-11261133-main',
+});
