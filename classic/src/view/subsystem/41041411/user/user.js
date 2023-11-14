0d622d13881c4a80988e6fbcc5888fd0002c26a7
mity1999 <84894328+mity1999@users.noreply.github.com>
2023-11-14 13:13:30 +0800

    User Control Initial
    使用者介面初始化

--- classic/src/view/subsystem/41041411/user/user.js
+++ classic/src/view/subsystem/41041411/user/user.js
@@ -1,11 +1,11 @@
-Ext.define('antnex.subsystem.41041411.main.main',
+Ext.define('antnex.subsystem.41041411.user.user',
     {
         extend:'Ext.panel.Panel',
         requires:[
-        'antnex.subsystem.41041411.main.mainController',
+        'antnex.subsystem.41041411.user.userController',
         ],
-        alias:'widget.page-41041411-main',
-        controller:'page-41041411-main',
+        alias:'widget.page-41041411-user',
+        controller:'page-41041411-user',
         title:'41041411的首頁',
 
         items:[{
--- classic/src/view/subsystem/41041411/user/userController.js
+++ classic/src/view/subsystem/41041411/user/userController.js
@@ -1,4 +1,4 @@
-Ext.define('antnex.subsystem.41041411.main.mainController',{
+Ext.define('antnex.subsystem.41041411.user.userController',{
     extend:'Ext.app.ViewController',
-    alias:'controller.page-41041411-main'
+    alias:'controller.page-41041411-user'
 });
