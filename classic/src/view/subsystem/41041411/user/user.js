ff88aad7a5b2cc24dca56f4b4815bbdef6c2d4db
mity1999 <84894328+mity1999@users.noreply.github.com>
2023-11-14 13:10:18 +0800

    First_Backup
    First Code

---
+++ classic/src/view/subsystem/41041411/user/user.js
@@ -0,0 +1,16 @@
+Ext.define('antnex.subsystem.41041411.main.main',
+    {
+        extend:'Ext.panel.Panel',
+        requires:[
+        'antnex.subsystem.41041411.main.mainController',
+        ],
+        alias:'widget.page-41041411-main',
+        controller:'page-41041411-main',
+        title:'41041411的首頁',
+
+        items:[{
+            html:'41041411的首頁'
+        }]
+
+    }
+);
