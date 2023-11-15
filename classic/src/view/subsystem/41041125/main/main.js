72f29129aad7d66cbe78542015e76962e0f7186c
Kappawind <148867662+Kappawind@users.noreply.github.com>
2023-11-15 17:12:54 +0800

    1114HW

---
+++ classic/src/view/subsystem/41041125/main/main.js
@@ -0,0 +1,14 @@
+Ext.define('antnex.subsystem.41041125.main.main',{
+    extend: 'Ext.panel.Panel',
+    requires: [
+        'antnex.subsystem.41041125.main.mainController',
+    ],
+    alias: 'widget.page-41041125-main',
+    controller: 'page-41041125-main',
+
+    title: '41041125首頁',
+    items: [{
+        html: '41041125首頁'
+    }]
+    
+});
---
+++ classic/src/view/subsystem/41041125/main/mainController.js
@@ -0,0 +1,5 @@
+Ext.define('antnex.subsystem.41041125.main.mainController',
+    {
+    extend: "Ext.app.ViewController",
+    alias: 'controller.page-41041125-main',
+});    
