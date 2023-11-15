2469fbd138b444f55f914dca2e7f3a663c9a979b
stanleylee417 <stanleylee417@gmail.com>
2023-11-15 15:06:16 +0800

    Revert "Merge remote-tracking branch 'origin/41041116' into Stanley"
    This reverts commit 2a4896f8db79979fab52b23b3524eff2e0b8edb7, reversing
    changes made to 71df3b5db61a6d2cb39f548f32f233106672b17f.

--- classic/src/view/subsystem/41041116/SampleController.js
+++
@@ -1,8 +0,0 @@
-Ext.define('antnex.subsystem.sample.SampleController', {
-    extend: 'antnex.default.defaultController',
-    alias: 'controller.sample-sample',
-    requires: [],
-    config: {
-        name: '範例頁面',
-    },
-});
