5590320c23636627c5890617aabdfac8209ac614
HCVG23 <choubenson71@gmail.com>
2023-11-20 18:26:51 +0800

    使用者管理

---
+++ classic/src/view/subsystem/41141127/main/main.js
@@ -0,0 +1,12 @@
+Ext.define('antnex.subsystem.41141127.main.main',{
+   extend:'Ext.panel.Panel',
+   requires:[
+        'antnex.subsystem.41141127.main.mainController',
+   ],
+   alias:'widget.page-41141127-main',
+   controller:'page-41141127-main',
+   title:'41141127首頁',
+   items:[{
+        html:'41141127首頁'
+   }] 
+});
---
+++ classic/src/view/subsystem/41141127/main/mainController.js
@@ -0,0 +1,8 @@
+Ext.define('antnex.subsystem.41141127.main.mainController',{
+    extend:'Ext.app.ViewController',
+    alias:'controller.page-41141127-main',
+    requires: [],
+    config: {
+        name: '41141127的首頁',
+    },
+});
