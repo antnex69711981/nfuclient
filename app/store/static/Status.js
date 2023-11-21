a6e0f003fc69ebe0d4e212c7fbbda5b242f6337b
fantralee <103614769+fantralee@users.noreply.github.com>
2023-11-22 01:53:33 +0800

    reverse

--- app/store/static/Status.js
+++ app/store/static/Status.js
@@ -2,7 +2,6 @@ Ext.define('antnex.store.static.Status', {
     extend: 'Ext.data.Store',
     alias: 'store.status',
     data: [
-        //{ value:-1, text: '全部', color: 'blue' },
         { value: 1, text: '啟用', color: 'green'},
         { value: 9, text: '停用', color: 'red'  },
     ],
