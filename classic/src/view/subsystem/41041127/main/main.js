c06a3c941539ca197583e8e1a24f4da43ddd20c5
Thexushao <103614655+Thexushao@users.noreply.github.com>
2023-11-22 22:43:51 +0800

    使用範例製作使用者清單、查詢條件、資料維護功能

---
+++ classic/src/view/subsystem/41041127/main/main.js
@@ -0,0 +1,16 @@
+Ext.define('antnex.subsystem.41041127.main.main',
+    {
+        extend: 'antnex.default.defaultView',
+        requires:[
+        'antnex.subsystem.41041127.main.mainController',
+        ],
+        alias:'widget.page-41041127-main',
+        controller:'page-41041127-main',
+
+        title: '41041127首頁',
+
+        items:[{
+            html: '41041127首頁'
+        }]
+    }
+);
