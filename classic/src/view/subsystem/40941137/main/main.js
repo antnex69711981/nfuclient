fb4d840de9f53aed358c75f3bfaaa12bdc1ff931
KEsrr <matterqazw@gmail.com>
2023-11-26 12:09:45 +0800

    完善功能

---
+++ classic/src/view/subsystem/40941137/main/main.js
@@ -0,0 +1,14 @@
+Ext.define('antnex.subsystem.40941137.main.main', {
+    extend:'Ext.panel.Panel',
+    requires:[
+        'antnex.subsystem.40941137.main.mainController',
+    ],
+    alias:'widget.page-40941137-main',
+    controller:'page-40941137-main',
+
+    title:'40941137首頁',
+
+    items:[{
+        html:'40941137首頁'
+    }]
+});
