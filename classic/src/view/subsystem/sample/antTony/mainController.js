495364da91d3e37bcec5a880a802795f2d93f2ab
stanleylee417 <stanleylee417@gmail.com>
2023-12-28 16:40:06 +0800

    init

--- classic/src/view/subsystem/sample/antTony/mainController.js
+++
@@ -1,8 +0,0 @@
-Ext.define('antnex.subsystem.sample.antTony.mainController', {
-    extend: 'antnex.default.defaultController',
-    alias: 'controller.antTony-main',
-    requires: [],
-    config: {
-        name: 'Tony的首頁',
-    },
-});
