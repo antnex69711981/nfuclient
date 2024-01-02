38211d7fab673850d02e850f533563e7cf0b4255
u6vup <144987464+u6vup@users.noreply.github.com>
2024-01-02 13:40:44 +0800

    新增維修項目UI

--- classic/src/view/subsystem/11261103/projectdetail/Status.js
+++
@@ -1,9 +0,0 @@
-Ext.define('antnex.subsystem.11261103.projectdetail.Status', {
-    extend: 'Ext.data.Store',
-    alias: 'store.status',
-    data: [
-        { value: 1, text: '未核佣'}, 
-        { value: 2, text: '已核佣'},
-        { value: 3, text: '不核佣'}, 
-    ],
-});
